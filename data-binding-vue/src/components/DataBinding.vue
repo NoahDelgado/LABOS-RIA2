dc4c792956745f5a90c145dd55081727b5bac601
NoahDelgado <noah.delgado1@eduvaud.ch>
2025-02-26 16:12:35 +0100

    feat: add DataBinding component with template and data properties

---
+++ data-binding-vue/src/components/DataBinding.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="name-container">
+    <p>{{ name }}</p>
+  </div>
+  <ul>
+    <li v-for="item in items" :key="item">{{ item }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4']
+    };
+  }
+};
+</script>
